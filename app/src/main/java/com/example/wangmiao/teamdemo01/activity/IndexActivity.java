8f0118489af2347d329389180f6a60ac769e360a
kezhijie1994 <1141606166@qq.com>
2016-11-01 15:11:17 +0800

    ，，

--- app/src/main/java/com/example/wangmiao/teamdemo01/activity/IndexActivity.java
+++ app/src/main/java/com/example/wangmiao/teamdemo01/activity/IndexActivity.java
@@ -27,7 +27,7 @@ public class IndexActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
         initView();
 
     }
