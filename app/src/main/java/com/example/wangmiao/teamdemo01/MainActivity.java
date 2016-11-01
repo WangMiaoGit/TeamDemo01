4d1481d16b7afff31e8587ad60036098e200cc3d
kezhijie1994 <1141606166@qq.com>
2016-11-01 11:19:36 +0800

    ，，

--- app/src/main/java/com/example/wangmiao/teamdemo01/MainActivity.java
+++ app/src/main/java/com/example/wangmiao/teamdemo01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wangmiao.teamdemo01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "梅儿子吃屎", Toast.LENGTH_SHORT).show();
         System.out.println("第一次提交的代码!!!");
         System.out.println("第二次提交的代码！");
         System.out.println("第三次提交!!!");
